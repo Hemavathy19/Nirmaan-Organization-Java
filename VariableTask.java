60ea15b6239c07a312836bba8808e0fd553bc91a
Hemavathy19 <149056928+Hemavathy19@users.noreply.github.com>
2025-04-03 15:47:10 +0530

    day2

---
+++ VariableTask.java
@@ -0,0 +1,28 @@
+package day2;
+
+public class VariableTask {
+	public static void main(String[] args) {
+	byte num = 120;
+	short a=3300;
+	int b=44444;
+	float c=12.34f;
+	double d=12.465d;
+	char f='a';
+	boolean isfalse=false;
+	System.out.println(num);
+	System.out.println(a);
+	System.out.println(b);
+	System.out.println(c);
+	System.out.println(d);
+	System.out.println(f);
+	System.out.println(isfalse);
+	
+	
+	
+	}
+	
+
+	
+	
+        
+}
